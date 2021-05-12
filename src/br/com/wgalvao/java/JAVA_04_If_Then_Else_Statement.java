434bfe12170b3850b8f5a981d3f515a376305917
Wagner Galvao <galvao.wagner@outlook.com.br>
2021-05-12 19:20:14 -0300

    Atualizado em 12/05/2021 19:20:00

--- src/br/com/wgalvao/java/JAVA_04_If_Then_Else_Statement.java
+++ src/br/com/wgalvao/java/JAVA_04_If_Then_Else_Statement.java
@@ -2,6 +2,7 @@ package br.com.wgalvao.java;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class JAVA_04_If_Then_Else_Statement {
 
@@ -9,12 +10,68 @@ public class JAVA_04_If_Then_Else_Statement {
 
 		// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/if.html
 
+		System.out.println("The if-then Statements");
+		System.out.println(" ");
+		String _message = "Fracassou";
+		boolean _comparison = true;
+		System.out.println("String _message = \"Fracassou\": " + _message);
+		System.out.println("boolean _comparison = true     : " + _comparison);
+		System.out.println("Sendo if (_comparison)         : " + _comparison);
+		if (_comparison) {
+			_message = "obteve Sucesso";
+			System.out.println("_message = \"obteve Sucesso\"  : " + _message);
+		}
+		System.out.println("Você " + _message);
+		System.out.println(" ");
+
+		_message = "Fracassou";
+		_comparison = false;
+		System.out.println("String _message = \"Fracassou\": " + _message);
+		System.out.println("boolean _comparison = false     : " + _comparison);
+		System.out.println("Não sendo if (_comparison)      : " + _comparison);
+		if (_comparison) {
+			_message = "obteve Sucesso";
+			System.out.println("_message = \"obteve Sucesso\"  : " + _message);
+		}
+		System.out.println("Você " + _message);
+		System.out.println(" ");
+
+		System.out.println("The if-then-else Statements");
+		System.out.println(" ");
+		Random gerador = new Random();
+		int _number = gerador.nextInt(100);
+		String _start = (_number%2 == 0) ? "Sendo" : "Não sendo";
+		System.out.println("_number: " + _number);
+		System.out.println(_start + " if (_number%2 == 0)            : " + (_number%2 == 0));
+		if (_number%2 == 0) {
+			_message = " par";
+			System.out.println("Executa _message = \" par\" do bloco then: " + _message);
+		} else {
+			_message = " ímpar";
+			System.out.println("Executa _message = \" ímpar\" do bloco else: " + _message);
+		}
+		System.out.println(" ");
+
+		_number++;
+		System.out.println("O número " + _number + " é" + _message);
+		_start = (_number%2 == 0) ? "Sendo" : "Não sendo";
+		System.out.println("_number: " + _number);
+		System.out.println(_start + " if (_number%2 == 0)            : " + (_number%2 == 0));
+		if (_number%2 == 0) {
+			_message = " par";
+			System.out.println("Executa _message = \" par\" do bloco then: " + _message);
+		} else {
+			_message = " ímpar";
+			System.out.println("Executa _message = \" ímpar\" do bloco else: " + _message);
+		}
+		System.out.println("O número " + _number + " é" + _message);
+        System.out.println(" ");
+		System.out.println("The if-then-else if-else Statements");
+		System.out.println(" ");
 		LocalDateTime agora = LocalDateTime.now();
 		DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
 		String _horaFormatada = formatterHora.format(agora);
 		int _hora = agora.getHour();
-		System.out.println("The if-then and if-then-else Statements");
-		System.out.println(" ");
 		System.out.println("Agora são " + _horaFormatada);
 		String _saudacao;
 
